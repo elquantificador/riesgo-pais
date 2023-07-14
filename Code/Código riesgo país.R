881262d4407f3144c006cfb3db0e6e7be5959e03
Andres <esoria@estud.usfq.edu.ec>
2023-07-14 10:18:00 -0500

    Fix error carpetas

--- Code/Código riesgo país.R
+++ Code/Código riesgo país.R
@@ -3,7 +3,7 @@ library(readxl)
 library(ggplot2)
 library(lubridate)
 #Importar base
-Puntos_riesgo_país <- read_excel("Puntos riesgo país.xlsx")
+Puntos_riesgo_país <- read_excel("Data/Puntos riesgo país.xlsx")
 
 #Creación de tema
 theme_article_rp <-
@@ -31,7 +31,23 @@ data <- data.frame(Periodo = periodo, A.fin.de.periodo = a_fin_periodo)
 # Convertir a formato fecha
 data$Periodo <- as.Date(data$Periodo, format = "%d/%m/%Y")
 
-                      
+# Definir las coordenadas de la zona a pintar
+zona_xmin <- as.Date("01/10/2019", format = "%d/%m/%Y")
+zona_xmax <- as.Date("01/12/2019", format = "%d/%m/%Y")
+zona_ymin <- 0
+zona_ymax <- 5500
+zona_xmin1 <- as.Date("01/02/2020", format = "%d/%m/%Y")
+zona_xmax1 <- as.Date("01/04/2020", format = "%d/%m/%Y")
+
+zona_xmin2 <- as.Date("01/01/2021", format = "%d/%m/%Y")
+zona_xmax2 <- as.Date("01/04/2021", format = "%d/%m/%Y")
+
+zona_xmin3 <- as.Date("01/05/2022", format = "%d/%m/%Y")
+zona_xmax3 <- as.Date("01/09/2022", format = "%d/%m/%Y")
+
+zona_xmin4 <- as.Date("01/01/2023", format = "%d/%m/%Y")
+zona_xmax4 <- as.Date("01/03/2023", format = "%d/%m/%Y")
+
 # Gráfico de líneas para promedio y puntos (1)
 gráfico_riesgo <- ggplot(data, aes(x = Periodo, y= A.fin.de.periodo)) +
   geom_line(colour = 'black') +
@@ -42,20 +58,34 @@ gráfico_riesgo <- ggplot(data, aes(x = Periodo, y= A.fin.de.periodo)) +
        y = 'Puntos base',
        title = 'Riesgo País Ecuador periodo 2019-2023',
        subtitle = 'Puntos básicos mensuales',
-       caption = 'Nota: Fuente: Banco Central del Ecuador 
-       El gráfico muestra la evolución del riesgo país en Ecuador durante el periodo de 2019 a 2023. 
-       Los puntos base representan la diferencia entre la tasa de interés de los bonos soberanos del país 
-       y la tasa de interés libre de riesgo.Los datos utilizados en este gráfico fueron obtenidos del Banco Central del Ecuador.') +
+       caption = 'Nota: Fuente: Banco Central del Ecuador. 
+       El gráfico muestra la evolución del riesgo país en Ecuador durante el periodo de 2019 a 2023. Los puntos base representan la diferencia entre la tasa de 
+       interés de los bonos soberanos del país y la tasa de interés libre de riesgo. Muestra el panorama económico para la inversión extranjera La primera zona 
+       roja del gráfico está asociada a un aumento en el valor del riesgo debido a la crisis política causada por el paro nacional en octubre del 2019.La 
+       segunda zona roja está asociada al inicio de la pandemia COVID-19 a finales de febrero. En el primer cuatrimestre del año 2021 hay una bajada que se 
+       debe al posicionamiento del nuevo gobierno con Guillermo Lasso como presidente. A finales de mayo del 2022 existe una subida que está ligada a otro 
+       paro nacional provocado por los precios de los combustibles. Por último, la subida a finales de enero del 2023 se debe principalmente a la inestabilidad 
+       del gobierno donde se pedía la renuncia del presidente que actualmente ha llevado a la muerte cruzada y la disolución de la asamblea.') +
   theme_article_rp +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5),
         axis.text.y = element_text(size = 8),
         plot.title = element_text(face = 'bold'),
         plot.subtitle = element_text(size = 12),
-        plot.caption = element_text(size = 8))
+        plot.caption = element_text(size = 8))+
+  annotate('rect', xmin = zona_xmin, xmax = zona_xmax, ymin = zona_ymin, ymax = zona_ymax,
+      alpha = 0.1, fill = 'red')+
+  annotate('rect', xmin = zona_xmin1, xmax = zona_xmax1, ymin = zona_ymin, ymax = zona_ymax,
+           alpha = 0.1, fill = 'red')+
+  annotate('rect', xmin = zona_xmin2, xmax = zona_xmax2, ymin = zona_ymin, ymax = zona_ymax,
+           alpha = 0.1, fill = 'green')+
+  annotate('rect', xmin = zona_xmin3, xmax = zona_xmax3, ymin = zona_ymin, ymax = zona_ymax,
+           alpha = 0.1, fill = 'red')+
+  annotate('rect', xmin = zona_xmin4, xmax = zona_xmax4, ymin = zona_ymin, ymax = zona_ymax,
+           alpha = 0.1, fill = 'red')
 
-ggsave(filename = "grafico_riesgo_pais.png", 
+ggsave(filename = "Figures/grafico_riesgo_pais.png", 
        plot = gráfico_riesgo, 
-       width = 8, height = 6, dpi = 300)
+       width = 9, height = 7, dpi = 300)
 
 
 
