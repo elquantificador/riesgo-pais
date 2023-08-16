61b4dffa67fa15a17008df27832a58cf505bc034
Andres <esoria@estud.usfq.edu.ec>
2023-08-16 12:29:14 -0500

    Modificaciones estéticas

--- Code/Gráfico Dinámico RP.R
+++ Code/Gráfico Dinámico RP.R
@@ -1,20 +1,22 @@
 # Cargamos las librerías necesarias
 library(shiny)
-library(plotly)
 library(highcharter)
 library(readxl)
 
 # Definimos la UI para la aplicación
 ui <- fluidPage(
-  titlePanel("Puntos básicos del riesgo país Ecuador"),
+  titlePanel("Riesgo país Ecuador 2004-2023"),
   
   sidebarLayout(
     sidebarPanel(
       dateRangeInput('dateRange',
-                     label = 'Seleccione un rango de fechas',
+                     label = 'Filtrado dinámico por fechas',
                      start = as.Date("2004-07-01"), 
                      end = as.Date("2023-06-01")
-      )
+      ),
+      helpText("Fuente: Banco Central del Ecuador. Los puntos base del riesgo país representan la diferencia 
+               entre la tasa de rendimiento de la deuda de un país y la tasa libre de riesgo. Un mayor número de 
+               puntos base indica un mayor riesgo percibido asociado con la inversión en ese país.")
     ),
     
     mainPanel(
@@ -38,26 +40,24 @@ server <- function(input, output) {
     df <- df[df$Periodo >= input$dateRange[1] & df$Periodo <= input$dateRange[2],]
     highchart() %>%
       hc_chart(type = "line") %>%
-      hc_title(text = "Análisis de riesgo país", style = list(fontSize = "24px", fontWeight = "bold")) %>%
-      hc_subtitle(text = "Filtrado por fechas", style = list(fontSize = "12px")) %>%
+      hc_title(text = "", style = list(fontSize = "24px", fontWeight = "bold")) %>%
+      hc_subtitle(text = "", style = list(fontSize = "12px")) %>%
       hc_xAxis(categories = df$Periodo, labels = list(rotation = -45)) %>%
-      hc_yAxis(title = list(text = "Riesgo país"), labels = list(style = list(fontSize = "8px"))) %>%
+      hc_yAxis(title = list(text = ""), labels = list(style = list(fontSize = "8px"))) %>%
       hc_plotOptions(
         series = list(
           color = 'black',
           marker = list(
             enabled = TRUE,
-            fillColor = 'black'
+            fillColor = 'black',
+            radius = 2
           )
         )
       ) %>%
-      hc_add_series(name = "Riesgo país", data = df$`Puntos_base`) %>%
-      hc_credits(enabled = TRUE, text = "Fuente: Datos de riesgo país", style = list(fontSize = "8px"))
-   
+      hc_add_series(name = "Riesgo país", data = df$`Puntos_base`)
   })
 }
 
 # Ejecutamos la aplicación 
 shinyApp(ui = ui, server = server)
-
 
